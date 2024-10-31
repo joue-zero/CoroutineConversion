aecbdf6d6426fd463bdb1ac48386e049128347cc
joue-zero <elnagaryoussef864@gmail.com>
2024-10-31 15:04:05 -0400

    using coroutinue

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,12 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
+
+
+
     val handler = Handler(Looper.getMainLooper(), Handler.Callback {
 
         currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
@@ -31,14 +40,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scope = CoroutineScope(Job()+ Dispatchers.IO)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+            scope.launch {
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    delay(40)
+                    withContext(Dispatchers.Main){
+                        //val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+
+                        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it)
+                        cakeImageView.alpha = it / 100f
+
+                    }
+                    //handler.sendEmptyMessage(it)
                 }
-            }.start()
+            }
         }
     }
+
 }
